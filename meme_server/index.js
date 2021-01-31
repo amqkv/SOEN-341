9c1b545aa8d181de8b736026abc8737aa4e8a6db
silentswordxv <silentswordxv@users.noreply.github.com>
2021-01-31 18:28:24 -0500

    Transition DB from local host to server host (mongoose)

--- meme_server/index.js
+++ meme_server/index.js
@@ -1,8 +1,38 @@
-//Index file :)
-import express from 'express';
+const express = require('express')
+const app = express()
+const port = 3000
 
-const app = express();
+const mongoose = require('mongoose');
 
-app.listen(3000, () =>
-    console.log('Listening on port 3000'),
-);
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:admin@cluster0.q6u0g.mongodb.net/testdab.341dbs?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+const user = require('./src/auth/authrepository.js');
+
+client.connect(err => {
+    const collection = client.db("testdab").collection("341dbs");
+    // perform actions on the collection object
+    const test_user = new user({username: 'test'});
+
+
+    client.close();
+});
+
+//mongoose.connect('mongodb+srv://admin:<password>@cluster0.q6u0g.mongodb.net/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+});
+
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log('Example app listening at http: localhost:${port}')
+        });
+
