20e1630f12faa37e778a89acebd367aa93af3dad
silentswordxv <silentswordxv@users.noreply.github.com>
2021-02-16 18:02:47 -0500

    #17 created homepage
    -Made homepage from template
    -Linked page to REPO and login page using react-router (no BackEnd)

--- client/src/Components/Templates/PostTemplate.js
+++ client/src/Components/Templates/PostTemplate.js
@@ -1,7 +1,6 @@
 //base template: https://material-ui.com/components/cards/
 
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -14,21 +13,13 @@ import IconButton from "@material-ui/core/IconButton";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import CardHeader from "@material-ui/core/CardHeader";
 
-
-const useStyles = makeStyles({
-    root: {
-        maxWidth: 400,
-    },
-});
-
 export default function Post(props) {
-    const classes = useStyles();
 
     return (
-        <Card className={classes.root}>
+        <Card className="post_card">
             <CardHeader
                 avatar={
-                    <Avatar aria-label="post" className={classes.avatar}>
+                    <Avatar aria-label="post" className="avatar">
                         user profile picture
                     </Avatar>
                 }
@@ -59,10 +50,13 @@ export default function Post(props) {
             </CardActionArea>
             <CardActions>
                 <Button size="small" color="primary">
-                    Share
+                    Like
                 </Button>
                 <Button size="small" color="primary">
-                    Learn More
+                    Follow
+                </Button>
+                <Button size="small" color="primary">
+                    Share
                 </Button>
             </CardActions>
         </Card>
