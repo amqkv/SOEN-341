e4f2cb8c49593791ee583561aa3b747a7da31ced
justpino7 <46535833+justpino7@users.noreply.github.com>
2021-02-25 00:37:38 -0500

    #32 function to display user profile stats

---
+++ client/src/Components/Templates/ProfileStats.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Templates.css';
+
+export default function profileStats(props) {
+    return (
+        <div id="mainBox">
+            <div id="leftBox">Posts: {props.posts}</div>
+            <div id="middleBox">Followers: {props.followers}</div>
+            <div id="rightBox">Following: {props.following}</div>
+        </div>
+    );
+}
