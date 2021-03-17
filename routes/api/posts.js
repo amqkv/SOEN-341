ec4455e2a8eac6357acb8309a3160b703026e6c4
Ion Balan <balan1541@gmail.com>
2021-03-17 19:55:59 -0400

    #35 Feed prioritizes posts from followed users

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,11 +7,13 @@ const aws = require('aws-sdk')
 const multerS3 = require('multer-s3');
 const multer = require('multer');
 const Post = require("../../models/Post");
+const User = require("../../models/User");
 const Comment = require("../../models/Comment");
 
 require('dotenv/config');
-const FormData = require('form-data');
 
+const max_posts_in_feed = 10
+const FormData = require('form-data');
 const validateAddPostInput = require("../../validation/addPost");
 // const { default: Comments } = require("../../client/src/Components/Templates/Post/Comments");
 
@@ -23,15 +25,6 @@ const s3 = new aws.S3({ apiVersion: '2006-03-01',
 
 const upload = multer ({ storage: multerS3({ s3: s3, bucket: process.env.S3_BUCKET }) }); //Fixed
 
-const fetch_10_post_from_db = function(req,res){
-    Post.find({}, function (err,posts) {
-        if (err){
-            res.send('Unable to retrive posts')
-            next();
-        }
-        res.json(posts);
-    });
-}
 
 //Post for addPost
 router.post("/addPost", (async function (req, res) {
@@ -59,71 +52,82 @@ router.post("/addPost", (async function (req, res) {
     }
 }));
 
-router.get("/getLatestPost",(async function (req, res) {
-    try {
+const fetch_posts_data = async function(array_of_posts){
+    
+    let s3_file_params = {};
+    let response = null;
+    let buffer = null;
+    let temp = null;
+    let post_data = [];
+    let comments = [];
+    let date = "";
 
-        let s3_file_params = {};
-        let response = null;
-        let buffer = null;
-        let temp = null;
-        let post_data = [];
-        let comments = [];
-        let date = "";
-        Post.find({}).sort({ date: -1 }).then(async function(posts) {
+    let i = 0;
+    for(i = 0; i < array_of_posts.length; i++){
 
-             for(const post of posts){
-                console.log("for loop")
-                console.log(post)
+        let post = array_of_posts[i];
+        
+        // Retrieve data from mongo DB
+        filekey = (post.S3Link).split('.com/')[1]
 
-                // Retrieve data from mongo DB
-                filekey = (post.S3Link).split('.com/')[1]
+        // Fetch image from S3
+        s3_file_params = { Bucket: process.env.S3_BUCKET, Key:filekey }
+        response = await s3.getObject(s3_file_params).promise();
 
-                // Fetch image from S3
-                s3_file_params = { Bucket: process.env.S3_BUCKET, Key:filekey }
-                response = await s3.getObject(s3_file_params).promise();
+        // Encode base64
+        buffer = Buffer.from(response["Body"]);
 
-                // Encode base64
-                buffer = Buffer.from(response["Body"]);
+        // Build response
+        temp = post["image"].split(".");
 
-                // Build response
-                temp = post["image"].split(".");
-
-                // Fetch comments from database
-                comments = await Comment.find({postID: post._id}).sort({ date: -1 });
-                // console.log(post["date"].toString().substring(0, post["date"].toString().indexOf("GMT") - 10))
-                date = post["date"].toString().substring(0, post["date"].toString().indexOf("GMT") - 10)
-                console.log(post["date"])
-                console.log(date)
-                post_data.push({
-                    "postID": post["_id"],
-                    "ownerID":post["ownerID"],
-                    "image": { "name":post["image"], "encoding":temp[temp.length -1],"file":buffer.toString("base64")},
-                    "description":post["description"],
-                    "date": date,
-                    "comments": comments,
-                    "username": post["username"]
-                })
-            }
-            res.json(post_data)
+        // Fetch comments from database
+        comments = await Comment.find({postID: post._id}).sort({ date: -1 });
+        // console.log(post["date"].toString().substring(0, post["date"].toString().indexOf("GMT") - 10))
+        date = post["date"].toString().substring(0, post["date"].toString().indexOf("GMT") - 10)
+        post_data.push({
+            "postID": post["_id"],
+            "ownerID":post["ownerID"],
+            "image": { "name":post["image"], "encoding":temp[temp.length -1],"file":buffer.toString("base64")},
+            "description":post["description"],
+            "date": date,
+            "comments": comments,
+            "username": post["username"]
         })
-
-        } catch(err) {
-        // An error occurred when fetching 
-        console.log(err)
-        return
     }
-}));
+
+    return post_data
+}
 
 router.get("/getFeed",(async function (req, res) {
     try {
 
-        // TODO 
-        // fetch list of followed users
-        // figure out how many posts to fetch from each
-        // sort all fetched posts by date
-        // Take the N most recent
-        // Fetch N images from S3
-        // Return a JSON array payload
+        // Get user data
+        var user_data = await User.find({ '_id': req.query.userID });
+
+        // Perpare following list date limit
+        var now = new Date(req.query.forwardDateLimit)
+        var two_days_ago = new Date(now.setDate(now.getDate()-2));
+
+        // Fetch posts of followers within the last 2 days
+        var relevant_posts = await Post.find({'username': { $in: user_data[0].following }, 'date':{ $lte: two_days_ago}}).sort({ date: -1 }).limit(max_posts_in_feed);
+
+        // If the amount of posts smaller than limit, fetch random feeds to compensate
+        if ( relevant_posts.length < 10 ){
+
+            var random_recent_posts = []
+            var selected_post_ids = []
+            let i = 0
+
+            for (i = 0; i < relevant_posts.length; i++){
+                selected_post_ids.push(relevant_posts[i]['_id'])
+            }
+            
+            random_recent_posts = await Post.find({'_id': { $nin: selected_post_ids }}).sort({ date: -1 }).limit(max_posts_in_feed - relevant_posts.length);
+            relevant_posts = relevant_posts.concat(random_recent_posts);
+
+        }
+        var posts_data = await fetch_posts_data(relevant_posts)
+        res.json(posts_data)
 
     } catch(err) {
         // An error occurred when fetching 
